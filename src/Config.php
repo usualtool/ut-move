c3a676c67d9c33699e58af9b6b26618c57f82a43
usualtool <52517593+usualtool@users.noreply.github.com>
2022-05-05 01:05:04 +0800

    Update and rename index.php to Config.php

---
+++ src/Config.php
@@ -0,0 +1,13 @@
+<?php
+$sync=[
+    "ftp_serv"=>"127.0.0.1",
+    "ftp_port"=>21,
+    "ftp_user"=>"test",
+    "ftp_pass"=>"123456",
+    "ftp_pasv"=>0,
+    "db_serv"=>"localhost",
+    "db_port"=>3306,
+    "db_name"=>"",
+    "db_user"=>"",
+    "db_pass"=>""
+];
